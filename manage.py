2462cb1e391ec5b24a0ee82195323a86ac4980a8
enobufs <yt0916@gmail.com>
2018-08-19 13:07:40 -0700

    No early terminate

--- manage.py
+++ manage.py
@@ -241,7 +241,7 @@ def train(cfg, tub_names, new_model_path, base_model_path=None ):
              saved_model_path=new_model_path,
              steps=steps_per_epoch,
              train_split=cfg.TRAIN_TEST_SPLIT,
-             use_early_stop=True)
+             use_early_stop=False)
 
 
 if __name__ == '__main__':
